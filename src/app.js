4b41a08c4059a5a0ee87f15d15104b040ce93fcb
capapra29 <capapra29@gmail.com>
2024-09-25 19:21:00 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function aleatoria(array) {
+    let palabra = array[Math.floor(Math.random() * array.length)];
+    return palabra;
+  }
+  document.getElementById("excuse").innerHTML =
+    aleatoria(who) +
+    " " +
+    aleatoria(action) +
+    " " +
+    aleatoria(what) +
+    " " +
+    aleatoria(when);
 };
